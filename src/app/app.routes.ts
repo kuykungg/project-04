c1f0ff3ec4c7ccf748a4c81d23e20c7cc350c6ae
kuykungg <zboybanz@outlook.com>
2024-09-10 13:25:00 +0700

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Component } from '@angular/core';
+export const routes: Routes = [
+  {
+    path: '',
+    component: FrontLayoutComponent,
+    children: [
+      { path: '', component:HomeComponent},
+      {path: 'about', component:AboutComponent},
+      {path: 'login', component:LoginComponent},
+      {path: 'register', component:RegisterComponent}
+
+    ]
+
+  }
+];
---
+++ src/app/layouts/front-layout/front-layout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../navbar/navbar.component";
+import { AppComponent } from '../../app.component';
+import { FooterComponent } from "../../footer/footer.component";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-front-layout',
+  standalone: true,
+  imports: [NavbarComponent,AppComponent, FooterComponent ,RouterOutlet],
+  templateUrl: './front-layout.component.html',
+  styleUrl: './front-layout.component.scss'
+})
+export class FrontLayoutComponent {
+
+}
